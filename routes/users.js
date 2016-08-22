3571dc4dbe2ec0dc026fc52024c08a24a0ebafb5
kevinw123 <kevinwong_895@hotmail.com>
2016-08-22 14:16:48 -0700

    errors on invalid registration

--- routes/users.js
+++ routes/users.js
@@ -11,5 +11,30 @@ router.get('/login', function(req, res){
 	res.render('login');
 });
 
+//Register user
+router.post('/register', function(req, res){
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+
+	//Validation
+	req.checkBody('name', 'Name is required').notEmpty();
+	req.checkBody('email', 'Email is required').notEmpty();
+	req.checkBody('email', 'Email is not valid').isEmail();
+	req.checkBody('username', 'Username is required').notEmpty();
+	req.checkBody('password', 'Password is required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+	var errors = req.validationErrors();
+	if(errors){
+		res.render('register', {
+			errors: errors
+		});
+	}else{
+		console.log('PASSED');
+	}
+});
 
 module.exports = router;
