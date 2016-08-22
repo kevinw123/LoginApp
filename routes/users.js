64218b65be56078e544697701742fd4136a3c3ef
kevinw123 <kevinwong_895@hotmail.com>
2016-08-22 14:59:56 -0700

    Adding registration functionality

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var User = require('../models/user');
 //Register
 router.get('/register', function(req, res){
 	res.render('register');
@@ -33,7 +33,20 @@ router.post('/register', function(req, res){
 			errors: errors
 		});
 	}else{
-		console.log('PASSED');
+		var newUser = new User({
+			name: name,
+			email: email,
+			username: username,
+			password: password
+		});
+
+		User.createUser(newUser, function(err, user){
+			if(err) throw err;
+			console.log(user);
+		});
+
+		req.flash('success_msg', 'You are registered and can now login');
+		res.redirect('/users/login');
 	}
 });
 
